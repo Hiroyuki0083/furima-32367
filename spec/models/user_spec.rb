f9559c33ea33179d08f0bbdbcd9ed965c1d96b5b
hiroyuki0083 <karasawa5000@icloud.com>
2021-03-20 18:07:17 +0900

    factoryBot

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
- RSpec.describe User, type: :model do
-   describe "ユーザー新規登録" do
+
+RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+  
+  describe "ユーザー新規登録" do
      it "nicknameが空だと登録できない" do
       user = User.new(nickname: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
       user.valid?
@@ -9,7 +14,29 @@ require 'rails_helper'
      it "emailが空では登録できない" do
       user = User.new(nickname: "ueno", email: "", password: "00000000", password_confirmation: "00000000")
       user.valid?
-      expect(user.errors.full_messages).to include("Nickname can't be blank")
+      expect(user.errors.full_messages).to include("Email can't be blank")
      end
-   end
- end
+     it 'emailに@が含まれれば登録できる' do
+     end
+     it 'passwordが空では登録できない' do
+      user = User.new(nickname: "ueno", email: "kkk@gmail.com", password: "", password_confirmation: "")
+      user.valid?
+      expect(user.errors.full_messages).to include("Password can't be blank")
+     end
+     it 'passwordは確認用を含めて2回入力する' do
+     end
+     it '重複したemailが存在する場合登録できない' do
+      @user.save
+      another_user = FactoryBot.build(:user)
+      another_user.email = @user.email
+      another_user.valid?
+      expect(another_user.errors.full_messages).to include('Email has already been taken')
+     end
+     it 'passwordが5文字以下では登録できない' do
+      @user.password = '00000'
+      @user.password_confirmation = '00000'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+     end
+  end
+end
