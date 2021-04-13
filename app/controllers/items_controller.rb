bcaebb3eefddeea1a5f10c9dcce3b8548053fcb3
hiroyuki0083 <karasawa5000@icloud.com>
2021-04-13 23:17:38 +0900

    削除ボタンにパス指定、コントローラー、削除後の画面

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -32,6 +32,11 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+  end
+
   private
   
   def items_params
