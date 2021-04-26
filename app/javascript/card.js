0e5ca1599fe540352134164c35cf4bd48ddb1129
hiroyuki0083 <karasawa5000@icloud.com>
2021-04-26 21:40:43 +0900

    STEP2　手順１

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -8,28 +8,28 @@ const pay = () => {
     const formData = new FormData(formResult);
 
     const card = {
-      number: formData.get("order[number]"),
-      cvc: formData.get("order[cvc]"),
-      exp_month: formData.get("order[exp_month]"),
-      exp_year: `20${formData.get("order[exp_year]")}`,
+      number: formData.get("form[number]"), // カッコ内にはName属性を入力する。
+      cvc: formData.get("form[cvc]"),
+      exp_month: formData.get("form[exp_month]"),
+      exp_year: `20${formData.get("form[exp_year]")}`,
     };
-    console.log("フォーム送信時にイベント発火")
-    console.log(card)
 
     Payjp.createToken(card, (status, response) => {
       if (status == 200) {
         const token = response.id;
-        const renderDom = document.getElementById("charge-form");
-        const tokenObj = `<input value=${token} name='token' type="hidden">`;
-        renderDom.insertAdjacentHTML("beforeend", tokenObj);
+        console.log(token)
+      //   const renderDom = document.getElementById("charge-form");
+      //   const tokenObj = `<input value=${token} name='token' type="hidden">`;
+      //   renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
 
-      document.getElementById("card-number").removeAttribute("name");
-      document.getElementById("card-cvc").removeAttribute("name");
-      document.getElementById("card-exp-month").removeAttribute("name");
-      document.getElementById("card-exp-year").removeAttribute("name");
+      // document.getElementById("card-number").removeAttribute("name");
+      // document.getElementById("card-cvc").removeAttribute("name");
+      // document.getElementById("card-exp-month").removeAttribute("name");
+      // document.getElementById("card-exp-year").removeAttribute("name");
       // 25−28行目はサーバーサイドにカードの情報を送信させず、削除する記述
       // document.getElementById("charge-form").submit();
+      // ↑サーバーに送られた後はコンソールの表記がクリアされる。確認したい場合はコメントアウト
 
     });
   });
