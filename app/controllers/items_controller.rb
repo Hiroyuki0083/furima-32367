f15aae989b0192886b4ed7fb5b95c545fc006ae6
hiroyuki0083 <karasawa5000@icloud.com>
2021-04-30 16:01:48 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -51,7 +51,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    if @item.user_id != current_user.id || @item.buy_management ！= nil
+    if @item.user_id != current_user.id || @item.buy_management ！！= nil
       redirect_to root_path
     end
   end
