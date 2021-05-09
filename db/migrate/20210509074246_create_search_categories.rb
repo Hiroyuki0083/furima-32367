ee8459c8e74ca19597df9f8a33625895c43488fd
hiroyuki0083 <karasawa5000@icloud.com>
2021-05-09 20:48:52 +0900

    テーブル作成

--- db/migrate/20210509074246_create_search_categories.rb
+++ db/migrate/20210509074246_create_search_categories.rb
@@ -1,7 +1,8 @@
 class CreateSearchCategories < ActiveRecord::Migration[6.0]
   def change
     create_table :search_categories do |t|
-
+      t.string      :name, null: false
+      
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_30_124603) do
+ActiveRecord::Schema.define(version: 2021_05_09_081108) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -70,6 +70,23 @@ ActiveRecord::Schema.define(version: 2021_04_30_124603) do
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
+  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "size", null: false
+    t.string "status", null: false
+    t.integer "price", null: false
+    t.bigint "search_category_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["search_category_id"], name: "index_products_on_search_category_id"
+  end
+
+  create_table "search_categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -93,4 +110,5 @@ ActiveRecord::Schema.define(version: 2021_04_30_124603) do
   add_foreign_key "buy_managements", "items"
   add_foreign_key "buy_managements", "users"
   add_foreign_key "items", "users"
+  add_foreign_key "products", "search_categories"
 end
