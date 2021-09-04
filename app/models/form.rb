4e4315fcf54f3017becb3e58932425cc490361a6
hiroyuki0083 <karasawa5000@icloud.com>
2021-09-04 23:39:22 +0900

    改行

--- app/models/form.rb
+++ app/models/form.rb
@@ -27,4 +27,5 @@ class Form
     buy_management = BuyManagement.create(item_id: item_id, user_id: user_id)
     Address.create(post_number: post_number, shipping_area_id: shipping_area_id, municipality: municipality, address: address, building_name: building_name, telephone_number: telephone_number, buy_management_id: buy_management.id)
   end
+  
 end
